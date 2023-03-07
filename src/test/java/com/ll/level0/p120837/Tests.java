4816a0041d64b8673a3924962845e7361a4d8780
seongmin8636 <moss3004@gmail.com>
2023-03-07 10:12:44 +0900

    re

---
+++ src/test/java/com/ll/level0/p120837/Tests.java
@@ -0,0 +1,32 @@
+package com.ll.level0.p120837;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Tests {
+    @Test
+    @DisplayName("23 => 5")
+    void t1() {
+        assertThat(new Solution().solution(23)).isEqualTo(5);
+    }
+
+    @Test
+    @DisplayName("24 => 6")
+    void t2() {
+        assertThat(new Solution().solution(24)).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("999 => 201")
+    void t3() {
+        assertThat(new Solution().solution(999)).isEqualTo(201);
+    }
+
+    @Test
+    @DisplayName("552 => 112")
+    void t4() {
+        assertThat(new Solution().solution(552)).isEqualTo(112);
+    }
+}
